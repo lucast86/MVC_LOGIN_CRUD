997f5ad790e2d8d6a9a512b355b9983c2ee96b4b
Lucas Trento <lucastrento86@gmail.com>
2020-12-01 00:21:53 -0300

    Agregar archivos de proyecto.

---
+++ MVC_LOGIN_CRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_LOGIN_CRUD.MvcApplication" Language="C#" %>
---
+++ MVC_LOGIN_CRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_LOGIN_CRUD.Startup))]
+namespace MVC_LOGIN_CRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
